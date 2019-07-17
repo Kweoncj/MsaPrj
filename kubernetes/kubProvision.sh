8271d47f79f6f8ff1c7a3260c06b7270c5067851
KweonCJ <kweoncj2019@gmail.com>
2019-07-17 17:00:17 +0900

    build Script and kubernetes script made.

--- kubernetes/kubProvision.sh
+++ kubernetes/kubProvision.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-kubectl delete deploy/nginx; kubectl run nginx --image=ckdwn28/nginx --port=80 -o yaml > /vagrant/MsaPrj/kubernetes/deploy.yaml
-kubectl scale deployment nginx --replicas=4
-kubectl expose deployment nginx --type="NodePort" --port 80
+#kubectl delete deploy/nginx; kubectl run nginx --image=ckdwn28/nginx --port=80 -o yaml > /vagrant/MsaPrj/kubernetes/deploy.yaml
+#kubectl scale deployment nginx --replicas=4
+# kubectl expose deployment nginx --type="NodePort" --port 80
+kubectl delete deploy/nginx
 kubectl create -f ./deploy.yaml
-kubectl expose deployment nginx --type="NodePort" --port 80
+
+kubectl delete svc/nginx
+kubectl create -f ./service.yaml
+# kubectl expose deployment nginx --type="NodePort" --port 80
